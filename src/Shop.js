da461dbadd12b171e1a19f5b71a9419d041df0d0
Jason Kim <jason.s.kim95@gmail.com>
2022-01-10 17:00:18 -0500

    Create shop page with Etsy API data

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const [products, setProducts] = useState([]);
+
+  // fetch Etsy api data once when component mounts and store in state
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const apikey = "vv3qn3iae48c08suyjn5vnvt";
+    const response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://openapi.etsy.com/v3/application/listings/active?client_id=${apikey}&keywords=variegated_monstera`
+    );
+
+    console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      const listings = await data.results;
+      setProducts(listings);
+      console.log(listings);
+    } else {
+      console.log("oops");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Shop</h2>
+      <div className="products">
+        {products.map((product) => {
+          return (
+            <div
+              id={product.listing_id}
+              key={product.listing_id}
+              className="listing"
+            >
+              <div className="title">{product.title}</div>
+              <div className="price">
+                ${product.price.amount / product.price.divisor}
+              </div>
+              <a className="url" href={product.url}>
+                {product.url}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
