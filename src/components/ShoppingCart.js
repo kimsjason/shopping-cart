2be2784657728de923925ffd38cfafc57153d198
Jason Kim <jason.s.kim95@gmail.com>
2022-01-19 12:26:20 -0500

    Add wrappers / containers for cart formatting

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -15,56 +15,75 @@ const ShoppingCart = (props) => {
 
   console.log(props.cart);
   return (
-    <div className="shopping-cart hidden">
-      <h2 className="shopping-cart-header">My Cart</h2>
-      <div className="cart-items">
-        {props.cart.map((item) => {
-          return (
-            <div className="item">
-              <div className="title">{item.title}</div>
-              <div className="price">{item.price}</div>
-              <div className="quantity">
-                <button
-                  className="subtract-quantity"
-                  onClick={onSubtractItemQuantity}
-                >
-                  -
-                </button>
-                <input
-                  type="text"
-                  className="current-quantity"
-                  value={item.quantity}
-                  onChange={onUpdateQuantity}
+    <div className="sticky-wrapper">
+      <div className="shopping-cart hidden">
+        <h2 className="shopping-cart-header">My Cart</h2>
+        <div className="cart-items">
+          {props.cart.map((item) => {
+            return (
+              <div className="item">
+                <img
+                  className="listing-image"
+                  src={item.image}
+                  alt="Etsy listing"
                 />
-                <button className="add-quantity" onClick={onAddItemQuantity}>
-                  +
-                </button>
+                <div className="item-info">
+                  <div className="title">{item.title}</div>
+                  <div className="price">${item.price}</div>
+                  <div className="quantity">
+                    <button
+                      className="subtract-quantity"
+                      onClick={onSubtractItemQuantity}
+                    >
+                      -
+                    </button>
+                    <input
+                      type="text"
+                      className="current-quantity"
+                      value={item.quantity}
+                      onChange={onUpdateQuantity}
+                    />
+                    <button
+                      className="add-quantity"
+                      onClick={onAddItemQuantity}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
               </div>
+            );
+          })}
+        </div>
+        <div className="order-value">
+          <div className="subtotal">
+            <div classNAme="subtotal-header">Subtotal:</div>
+            <div className="amount">
+              $
+              {parseFloat(
+                props.cart.reduce((previousValue, currentItem) => {
+                  return (
+                    previousValue + currentItem.price * currentItem.quantity
+                  );
+                }, 0)
+              ).toFixed(2)}
             </div>
-          );
-        })}
-      </div>
-      <div className="subtotal">
-        Subtotal:
-        <div className="amount">
-          $
-          {parseFloat(
-            props.cart.reduce((previousValue, currentItem) => {
-              return previousValue + currentItem.price * currentItem.quantity;
-            }, 0)
-          ).toFixed(2)}
+          </div>
+          <div className="shipping">
+            <div className="shipping-header">Shipping:</div>
+            <div className="amount"> TBD</div>
+          </div>
+          <div className="tax">
+            <div className="tax-header">Tax:</div>{" "}
+            <div className="amount">TBD</div>
+          </div>
+          <div className="total">
+            <div className="total-header">Order Total:</div>{" "}
+            <div className="amount">TBD</div>
+          </div>
+          <button className="checkout">Checkout</button>
         </div>
       </div>
-      <div className="shipping">
-        Shipping: <div className="amount"> TBD</div>
-      </div>
-      <div className="tax">
-        Tax: <div className="amount">TBD</div>
-      </div>
-      <div className="total">
-        Order Total: <div className="amount"></div>
-      </div>
-      <button className="checkout">Checkout</button>
     </div>
   );
 };
