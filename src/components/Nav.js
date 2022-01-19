6ba8d9f7ca0bea9d1141f0febf1843a4b0b54f00
Jason Kim <jason.s.kim95@gmail.com>
2022-01-19 12:21:18 -0500

    Add home route to logo link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,26 +3,29 @@ import { Link } from "react-router-dom";
 import "../styles/Nav.css";
 
 const Nav = () => {
-  const handleClick = () => {
+  const handleToggleCart = () => {
     const shoppingCart = document.querySelector(".shopping-cart");
     shoppingCart.classList.toggle("hidden");
-    console.log(shoppingCart);
   };
 
   return (
     <nav>
-      <h2 className="logo">The Rare Plant Shop</h2>
-      <ul className="nav-links">
+      <div className="flex-wrapper">
         <Link to="/">
-          <li className="home">Home</li>
+          <h2 className="logo">The Rare Plant Shop</h2>
         </Link>
-        <Link to="/shop">
-          <li className="Shop">Shop</li>
-        </Link>
-        <li className="cart" onClick={handleClick}>
-          Shopping Cart
-        </li>
-      </ul>
+        <ul className="nav-links">
+          <Link to="/">
+            <li className="home">Home</li>
+          </Link>
+          <Link to="/shop">
+            <li className="Shop">Shop</li>
+          </Link>
+          <li className="cart" onClick={handleToggleCart}>
+            Shopping Cart
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
