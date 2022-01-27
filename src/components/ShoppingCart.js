a6bab5de2c23abe29109037f5cbd7f6aed9d0d1e
Jason Kim <jason.s.kim95@gmail.com>
2022-01-27 11:01:38 -0500

    Update shopping cart text

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,5 @@
 import "../styles/ShoppingCart.css";
+import Close from "@material-ui/icons/Close";
 
 const ShoppingCart = (props) => {
   const onSubtractItemQuantity = (e) => {
@@ -13,51 +14,69 @@ const ShoppingCart = (props) => {
     props.onUpdateQuantity(e);
   };
 
+  const onRemoveItem = (e) => {
+    props.onRemoveItem(e);
+  };
+
+  const handleToggleCart = () => {
+    const shoppingCart = document.querySelector(".shopping-cart");
+    shoppingCart.classList.toggle("hidden");
+  };
+
   return (
     <div className="sticky-wrapper">
       <div className="shopping-cart hidden">
-        <h2 className="shopping-cart-header">My Cart</h2>
-        <div className="cart-items">
-          {props.cart.map((item) => {
-            console.log(item);
-            return (
-              <div className="item" key={item.id} id={item.id}>
-                <img
-                  className="listing-image"
-                  src={item.images[0].url_fullxfull}
-                  alt="Etsy listing"
-                />
-                <div className="item-info">
-                  <div className="title">{item.title}</div>
-                  <div className="price">${item.price}</div>
-                  <div className="quantity">
-                    <button
-                      className="subtract-quantity"
-                      onClick={onSubtractItemQuantity}
-                    >
-                      -
-                    </button>
-                    <input
-                      type="text"
-                      className="current-quantity"
-                      value={item.quantity}
-                      onChange={onUpdateQuantity}
-                    />
-                    <button
-                      className="add-quantity"
-                      onClick={onAddItemQuantity}
-                    >
-                      +
-                    </button>
+        <div className="shopping-cart-header">
+          <h2 className="shopping-cart-title">My Cart</h2>
+          <Close className="close-cart" onClick={handleToggleCart} />
+        </div>
+        {props.cart.length === 0 ? (
+          <h3 className="cart-message">Your cart is empty!</h3>
+        ) : (
+          <div className="cart-items">
+            {props.cart.map((item) => {
+              return (
+                <div className="item" key={item.id} id={item.id}>
+                  <img
+                    className="listing-image"
+                    src={item.images[0].url_fullxfull}
+                    alt="Etsy listing"
+                  />
+                  <div className="item-info">
+                    <div className="title">{item.title}</div>
+                    <div className="price">${item.price}</div>
+                    <div className="quantity">
+                      <button
+                        className="subtract-quantity"
+                        onClick={onSubtractItemQuantity}
+                      >
+                        -
+                      </button>
+                      <input
+                        type="text"
+                        className="current-quantity"
+                        value={item.quantity}
+                        onChange={onUpdateQuantity}
+                      />
+                      <button
+                        className="add-quantity"
+                        onClick={onAddItemQuantity}
+                      >
+                        +
+                      </button>
+                      <div className="remove-item" onClick={onRemoveItem}>
+                        Remove
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        )}
         <div className="order-value">
           <div className="subtotal">
-            <div className="subtotal-header">Subtotal:</div>
+            <div className="subtotal-header">SUBTOTAL:</div>
             <div className="amount">
               $
               {parseFloat(
@@ -69,19 +88,18 @@ const ShoppingCart = (props) => {
               ).toFixed(2)}
             </div>
           </div>
-          <div className="shipping">
-            <div className="shipping-header">Shipping:</div>
-            <div className="amount"> TBD</div>
-          </div>
-          <div className="tax">
-            <div className="tax-header">Tax:</div>{" "}
-            <div className="amount">TBD</div>
-          </div>
-          <div className="total">
-            <div className="total-header">Order Total:</div>{" "}
-            <div className="amount">TBD</div>
-          </div>
-          <button className="checkout">Checkout</button>
+          <div className="shipping">Shipping calculated at checkout</div>
+        </div>
+        <div className="checkout-buttons">
+          <button className="checkout">Continue as a Guest</button>
+          <button className="sign-in">Sign In and Checkout</button>
+        </div>
+        <div className="payment-icons">
+          PAYMENT
+          <img
+            src={require("../assets/Payment-Icons.png")}
+            alt="payment-icons"
+          />
         </div>
       </div>
     </div>
