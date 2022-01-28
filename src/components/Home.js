54c9be696516d3ff298dbd0b45f52baaecad59c9
Jason Kim <jason.s.kim95@gmail.com>
2022-01-27 22:19:58 -0500

    Add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,26 @@
+import { ReactComponent as MonsteraOne } from "../assets/monstera-1.svg";
+import { ReactComponent as MonsteraTwo } from "../assets/monstera-2.svg";
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="image-container">
+          <MonsteraOne className="monstera-one" />
+          <MonsteraTwo className="monstera-two" />
+        </div>
+        <div className="hero-content">
+          <h1 className="main-logo">The Rare Plant Shop</h1>
+          <div className="message"></div>
+          <Link to="/shop">
+            <button className="shop-button">Start Browsing</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
